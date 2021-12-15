cb8e243d4d8401cecdfc42fbad8862511a8e8a4d
EthanPark <ethanpark604@gmail.com>
2021-12-16 05:36:11 +0900

    from private to public

--- KoreaTravelMasterApp/Resources/AppDelegate.swift
+++ KoreaTravelMasterApp/Resources/AppDelegate.swift
@@ -12,14 +12,14 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Thread.sleep(forTimeInterval: 2.0)
         
         FirebaseApp.configure()
-        
+
         return true
     }
 
